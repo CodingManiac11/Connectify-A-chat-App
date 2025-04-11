94e96dc945873299f15a6d279cbab42e988d7959
Aditya <adityautsav1901@gmail.com>
2025-04-11 15:05:38 +0530

    Connectify-A chat app

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,65 +1,38 @@
-import { Box, Container, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/layout";
 import { ChatState } from "../Context/ChatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import MyChats from "../components/MyChats";
 import Chatbox from "../components/Chatbox";
-import Login from "../components/Authentication/Login";
-import Signup from "../components/Authentication/Signup";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { Text } from "@chakra-ui/layout";
 
 const Homepage = () => {
   const { user } = ChatState();
+  const history = useHistory();
 
-  if (user) {
-    return (
-      <div style={{ width: "100%" }}>
-        <SideDrawer />
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          w="100%"
-          h="91.5vh"
-          p="10px"
-        >
-          <MyChats />
-          <Chatbox />
-        </Box>
-      </div>
-    );
-  }
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (!userInfo) history.push("/");
+  }, [history]);
 
   return (
-    <Container maxW="xl" centerContent>
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
       <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg="white"
+        display="flex"
+        justifyContent="space-between"
         w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
+        h="91.5vh"
+        p="10px"
       >
-        <Text fontSize="4xl" fontFamily="Work sans" textAlign="center">
-          Talk-a-tive
-        </Text>
+        {user && <MyChats />}
+        {user && <Chatbox />}
       </Box>
-      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-        <Tabs isFitted variant="soft-rounded">
-          <TabList mb="1em">
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Login />
-            </TabPanel>
-            <TabPanel>
-              <Signup />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
+      <Text fontSize="4xl" fontFamily="Work sans" textAlign="center">
+        Connectify
+      </Text>
+    </div>
   );
 };
 
--- mern-chat-app/backend/config/db.js
+++ mern-chat-app/backend/config/db.js
@@ -3,8 +3,13 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/chat-app");
-    console.log(`MongoDB Connected to: ${conn.connection.host}`.cyan.underline);
+    // Hardcoded local MongoDB connection
+    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/chat-app", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`MongoDB Connection Error: ${error.message}`.red.bold);
     process.exit(1);
